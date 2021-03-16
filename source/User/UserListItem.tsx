7c6f30e49d0ff2c52bcc5c2ed95f5a622beff197
ChitlangeSahas <chitlangesahas@gmail.com>
2021-03-15 20:25:13 -0700

    can show a basic user

--- source/User/UserListItem.tsx
+++ source/User/UserListItem.tsx
@@ -3,8 +3,8 @@ import ListItemText from '@material-ui/core/ListItemText'
 import ListItemAvatar from '@material-ui/core/ListItemAvatar'
 import Avatar from '@material-ui/core/Avatar'
 import ImageIcon from '@material-ui/icons/Image'
-import {ModristUser} from './types'
 import {ListItem} from '@material-ui/core'
+import {ModristUser} from './types'
 
 const UserListItem = ({name}: ModristUser): JSX.Element => {
   return (
