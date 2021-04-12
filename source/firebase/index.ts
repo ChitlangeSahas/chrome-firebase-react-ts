61225a094690b3410b000ed4b6c2d417ac0d0a1a
anngo-asu <ango5@asu.edu>
2021-04-12 12:31:41 -0700

    cleaned up

--- source/firebase/index.ts
+++ source/firebase/index.ts
@@ -20,33 +20,20 @@ console.log('Firebase config loaded')
 export const auth = firebase.auth()
 export const firestore = firebase.firestore()
 
-// const provider = new firebase.auth.GoogleAuthProvider()
 export const signInWithGoogle = () => {
-  /* auth.signInWithPopup(provider).then(()=>{
-    console.log('Pop up worked.')
-  }).catch((error)=>{
-    const errorCode = error.code
-    const errorMessage = error.message
-    const {email} = error
-    console.log('Error code: {0}; Error message: {1}; Email: {2}', errorCode, errorMessage, email)
-    }
-  ) */
-
+  // eslint-disable-next-line @typescript-eslint/ban-ts-comment
   // @ts-ignore
-  chrome.identity.getAuthToken((token: string | null | undefined)=>{
-    // @ts-ignore
+  chrome.identity.getAuthToken( (token: string) => {
     if(chrome.runtime.lastError){
       console.log('It was not possible to get a token programmatically.')
-    } else { // @ts-ignore
+    } else {
       // eslint-disable-next-line no-lonely-if
       if(chrome.runtime.lastError){
-            // @ts-ignore
         console.error(chrome.runtime.lastError)
           } else if(token){
-            const credential = firebase.auth.GoogleAuthProvider.credential(null, token)
+            const credential = firebase.auth.GoogleAuthProvider.credential(null, )
             firebase.auth().signInWithCredential(credential).catch((error)=>{
               if(error.code === 'auth/invalid-credential'){
-                // @ts-ignore
                 chrome.identity.removeCachedAuthToken({token}, ()=>{
                   signInWithGoogle()
                 })
